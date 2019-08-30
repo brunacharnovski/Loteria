74ac77918507ee74b7e363cfd418f56bef67e486
Bruna Charnovski <brunacharnovski@gmail.com>
2019-08-29 22:21:06 -0300

    Criado vetor para receber valores

--- Loteria/src/loteria/Loteria.java
+++ Loteria/src/loteria/Loteria.java
@@ -5,6 +5,18 @@ import java.util.Scanner;
 public class Loteria {
 
     public static void main(String[] args) {
+        int[] valores = new int[6];
+        imprimeVetor(valores);
+//        int[] valores2;
+//        valores2 = new int [6];
+//        valores2 [0] = 0;
+//        valores2 [1] = 0;
+//        valores2 [2] = 0;
+//        valores2 [3] = 0;
+//        valores2 [4] = 0;
+//        valores2 [5] = 0;
+        // para comentar a linha ou varias linhas ctrl + shift + c
+
         Scanner in = new Scanner(System.in);
         System.out.println("Digite 6 numeros");
         for (int i = 0; i < 6; i++) {
@@ -24,5 +36,12 @@ public class Loteria {
         } else {
             return -1;
         }
+    }
+
+    public static void imprimeVetor(int[] vet) {
+        for (int i = 0; i < vet.length; i++) {
+            System.out.print(vet[i] + " ");
+        }
+        System.out.println("");
     }
 }
