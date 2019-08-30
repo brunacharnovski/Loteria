ea76f9505a3132db4abcb7c1b238cd8a8049a2e4
Bruna Charnovski <brunacharnovski@gmail.com>
2019-08-29 21:50:15 -0300

    Aceitando apenas números válidos

--- Loteria/src/loteria/Loteria.java
+++ Loteria/src/loteria/Loteria.java
@@ -8,13 +8,21 @@ public class Loteria {
         Scanner in = new Scanner(System.in);
         System.out.println("Digite 6 numeros");
         for (int i = 0; i < 6; i++) {
-            capNum(in, i);
+            int num = -1
+            while (num == -1) {
+                num = capNum(in, i);
+            }
 
         }
     }
 
     public static int capNum(Scanner in, int i) {
-        System.out.println("Digite o " + (i+1) + "º numero");
-        return 0;
+        System.out.println("Digite o " + (i + 1) + "º numero");
+        int num = in.nextInt();
+        if (num >= 1 && num <= 60) {
+            return num;
+        } else {
+            return -1;
+        }
     }
 }
